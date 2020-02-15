6956c5543655bbf503fa794315fdbfc0b8ccef5f
Reggedict <48354875+Reggedict@users.noreply.github.com>
2020-02-15 08:14:52 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ def chatInfo(message):
 @bot.message_handler(commands=['rules'])
 def rules(message):
 	if message.chat.id==-1001317298639:
-		bot.forward_message(-1001317298639,-1001317298639,66)
+		bot.forward_message(-1001317298639,-1001317298639,215)
 
 
 
