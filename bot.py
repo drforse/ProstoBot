0a5e9185e978c6744c6546408c611e04f065cc99
Reggedict <48354875+Reggedict@users.noreply.github.com>
2020-03-01 18:59:07 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,8 +49,11 @@ def PinMessage(message):
 @bot.message_handler(commands=['unpin'])
 def unPinMessage(message):
 	user = bot.get_chat_member(message.chat.id, message.from_user.id)
-	if user.status != 'creator' and user.status != 'administrator':
+	if user.status == 'creator' and user.status == 'administrator':
+		bot.unpin_chat_message(message.chat.id)
 		bot.send_message(message.chat.id,"Успешно выполнено!")
+	else:
+		bot.send_message(message.chat.id,"Вы не администратор чата!")
 
 @bot.message_handler(commands=['infou'])
 def userInfo(message):
